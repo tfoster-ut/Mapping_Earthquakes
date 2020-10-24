bbe3c3d7a578e3615b1dc885cba7eb6e79765a73
Teale Foster <tealefoster@gmail.com>
2020-10-24 15:50:55 -0500

    resolve attempt to api_key

--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -5,12 +5,7 @@ console.log("working");
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-  // accessToken: API_KEY
-  await octokit.request('GET /repos/{tfoster-ut}/{Mapping_Earthquakes}/actions/permissions/selected-actions', {
-    owner: 'tfoster-ut',
-    repo: 'Mapping_Earthquakes',
-    github_owned_allowed: true
-  })
+  accessToken: API_KEY
 });
 
 // We create the second tile layer that will be the background of our map.
