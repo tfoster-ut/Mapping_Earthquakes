f6ad270fbbcf90d77d46a7a9fa158314574cc549
Teale Foster <tealefoster@gmail.com>
2020-10-24 15:33:40 -0500

    resolve attempt to api_key

--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -6,7 +6,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
   // accessToken: API_KEY
-  GET /repos/:tfoster-ut/:Mapping_Earthquakes/actions/secrets/:API_KEY
+  GET repos/:tfoster-ut/:Mapping_Earthquakes/actions/secrets/:API_KEY
 });
 
 // We create the second tile layer that will be the background of our map.
